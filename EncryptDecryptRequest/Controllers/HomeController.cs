7b89698a092cb1d2c594d2a9d0c6fff8c5e7a945
Vijay Sahu <vijay.sahu12@gmail.com>
2019-11-29 19:33:37 +0530

    First CheckIn

---
+++ EncryptDecryptRequest/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using EncryptDecryptRequest.EncryptionHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EncryptDecryptRequest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public string Message(string msg)
+        {
+            var objEncryptDecrypt = EncryptinDecryptionHelperFile.DecryptStringAES(msg);
+            
+            return objEncryptDecrypt;
+        }
+    }
+}
